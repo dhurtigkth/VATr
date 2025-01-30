3930a823596f23bdd420000a918489fccb86e782
dhurtigkth <100566590+dhurtigkth@users.noreply.github.com>
2025-01-30 11:29:17 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -134,5 +134,6 @@ if __name__ == '__main__':
     fakes = writer.generate(args.text)
     for i, fake in enumerate(fakes):
         dst_path = args.output.parent / (args.output.stem + f'_{i:03d}' + args.output.suffix)
+        print("PATH: ", str(dst_path))
         cv2.imwrite(str(dst_path), fake)
     print('Done')
