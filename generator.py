011352ea93adcfbd942565a06154442167dee5bc
dhurtigkth <100566590+dhurtigkth@users.noreply.github.com>
2025-01-30 11:34:04 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -134,7 +134,8 @@ if __name__ == '__main__':
     fakes = writer.generate(args.text)
     for i, fake in enumerate(fakes):
         dst_path = args.output.parent / (args.output.stem + f'_{i:03d}' + args.output.suffix)
+        dst_path = str(dst_path) + str(".png")
         print("HELLO")
-        print("PATH: ", str(dst_path + ".png"))
-        cv2.imwrite(str(dst_path + ".png"), fake)
+        print("PATH: ", str(dst_path))
+        cv2.imwrite(str(dst_path), fake)
     print('Done')
